51b9c7e9086e3a3f5cc567582969badfa490df16
Fabian General <fabianwgl@gmail.com>
2017-02-03 14:06:37 -0300

    response with temp and hum

--- app/server/api/dht22/index.js
+++ app/server/api/dht22/index.js
@@ -2,12 +2,20 @@
 
 const config = require('../../../config');
 const express = require('express');
-
-
+var sensor = require('node-dht-sensor');
 let router = new express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('here');
+  sensor.read(22, 23, function(err, temperature, humidity) {
+    if (!err) {
+        //console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+        //    'humidity: ' + humidity.toFixed(1) + '%');
+        res.send('temp: ' + temperature.toFixed(1) + '°C, ' +
+            'humidity: ' + humidity.toFixed(1) + '%')
+    }else{
+    	res.send(404);
+    }
+  });
 });
 
 module.exports = router;
