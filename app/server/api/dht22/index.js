cb5677e94f392fed32b2816d76eec1a4e4a85daa
Fabian General <fabianwgl@gmail.com>
2017-02-16 19:29:43 -0300

    changed api response to json

--- app/server/api/dht22/index.js
+++ app/server/api/dht22/index.js
@@ -8,10 +8,11 @@ let router = new express.Router();
 router.get('/', (req, res, next) => {
   sensor.read(22, 23, function(err, temperature, humidity) {
     if (!err) {
-        //console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
-        //    'humidity: ' + humidity.toFixed(1) + '%');
-        res.send('temp: ' + temperature.toFixed(1) + '°C, ' +
-            'humidity: ' + humidity.toFixed(1) + '%')
+            
+        res.json({
+            temp:temperature.toFixed(1),
+            hum: humidity.toFixed(1)
+        });
     }else{
     	res.send(404);
     }
